afdf29fb0a5d9acf6dec7cd8e3fe33f5792d177b
Masazumi Oeda <blitz2002jp@yahoo.co.jp>
2018-11-28 12:18:42 +0900

    Initial Commit

---
+++ single/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  single
+//
+//  Created by ma on 2018/11/28.
+//  Copyright © 2018年 ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
